89e650349942c82e13ac152618e285f789d1db40
Mr:Coder <Deepakvm2024@gmail.com>
2025-09-27 18:54:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-     const video1 = document.getElementById('projectVideo1');
+      const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
 
